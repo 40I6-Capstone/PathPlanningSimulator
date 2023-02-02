3d78ace9a7c266c184959cb30aa5e2e0bfcf40c5
aronm99 <markanda@mcmaster.ca>
2023-02-02 11:16:58 -0500

    Add adaptive path creation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math;
 import numpy as np;
 import scipy.interpolate as si
+import scipy.spatial as ss
 import matplotlib.pyplot as plt
 colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
 
@@ -51,7 +52,7 @@ class pathPlanning:
 
 
         # calculate the angle the ugv's will leave from the origin for its own lane
-        d_angle = 2*math.asin((ugv_rad/2)/crit_rad);
+        d_angle = 2*math.asin((1.05*ugv_rad/2)/crit_rad);
 
         # calculate the radius of the inscribed circle for the shape
         radius = math.sqrt(math.pow((shape.midpoints[0][0]-shape.midpoints[int(num_of_paths/2)][0]),2)+math.pow((shape.midpoints[0][1]-shape.midpoints[int(num_of_paths/2)][1]),2))/2;
@@ -71,6 +72,7 @@ class pathPlanning:
             path2 = [];
             path3 = [];
             cv = [];
+            order = 1;
 
             if(i%2):
                 angle_off = i*d_angle;
@@ -82,9 +84,7 @@ class pathPlanning:
                 shape_i = int(i/2);
                 index_coef = i/2 + 0.2;
             
-            # TODO update how much the control points extends by
-            ctl_rad_ext = (0.5*radius+0.1*dist)*(index_coef);
-            ctl_spoke_ext = radius*0.2*(index_coef);
+            ctl_ext = self._get_ext_coef(radius, dist, index_coef, order);
 
             # calculate the points for inside the critical circle
             for j in range(nop_1):
@@ -95,13 +95,13 @@ class pathPlanning:
             cv.append([cr_x,cr_y]);
 
             # add the extension to the path 1 to the control points (off path)
-            cr_x_ext = (crit_rad + ctl_rad_ext)*math.cos((base_angle+angle_off));
-            cr_y_ext = (crit_rad + ctl_rad_ext)*math.sin((base_angle+angle_off));
+            cr_x_ext = (crit_rad + ctl_ext[0])*math.cos((base_angle+angle_off));
+            cr_y_ext = (crit_rad + ctl_ext[0])*math.sin((base_angle+angle_off));
             cv.append([cr_x_ext,cr_y_ext]);
             
             # add the extension to the spoke path to the control points (off path)
-            sp_x_ext = shape.midpoints[shape_i,0] + (spoke_len + ctl_spoke_ext)*shape.norms[shape_i,0];
-            sp_y_ext = shape.midpoints[shape_i,1] + (spoke_len + ctl_spoke_ext)*shape.norms[shape_i,1];
+            sp_x_ext = shape.midpoints[shape_i,0] + (spoke_len + ctl_ext[1])*shape.norms[shape_i,0];
+            sp_y_ext = shape.midpoints[shape_i,1] + (spoke_len + ctl_ext[1])*shape.norms[shape_i,1];
             cv.append([sp_x_ext,sp_y_ext]);
 
             #calculate the values for the spoke path
@@ -119,10 +119,35 @@ class pathPlanning:
             cv = np.array(cv);
             path2 = self._b_spline(cv);
 
+            if len(paths) >= 2:
+                min_path_dist =  ss.distance.cdist(path2, paths[-2]).min();
+                print("Min Path Distance", len(paths), min_path_dist)
+                while(min_path_dist < 2*ugv_rad):
+                    order += 1; 
+                    ctl_ext = self._get_ext_coef(radius, dist, index_coef, order);
+
+                    cr_x_ext = (crit_rad + ctl_ext[0])*math.cos((base_angle+angle_off));
+                    cr_y_ext = (crit_rad + ctl_ext[0])*math.sin((base_angle+angle_off));
+                    cv[1] = np.array([cr_x_ext,cr_y_ext]);
+
+                    sp_x_ext = shape.midpoints[shape_i,0] + (spoke_len + ctl_ext[1])*shape.norms[shape_i,0];
+                    sp_y_ext = shape.midpoints[shape_i,1] + (spoke_len + ctl_ext[1])*shape.norms[shape_i,1];
+                    cv[2] = np.array([sp_x_ext,sp_y_ext]);
+
+                    path2 = self._b_spline(cv);
+                    min_path_dist =  ss.distance.cdist(path2, paths[-2]).min();
+                    print("Min Path Distance", len(paths), min_path_dist)
+
             # concatenate the 3 path parts to get the complete path and add it to the path list 
             paths.append(path1 + path2 + path3);
         self.paths = np.array(paths);
-
+    
+    # function to get how much the control points should extend by from the anchor points
+    def _get_ext_coef(self, radius, dist, index_coef, order):
+            # TODO update how much the control points extends by
+            ctl_rad_ext = order*(0.2*radius + 0.05*dist + 0.5*index_coef);
+            ctl_spoke_ext = order*(0.2*radius + index_coef);
+            return np.array([ctl_rad_ext, ctl_spoke_ext]);
 
     def _b_spline(self, cv):
         n=1000;
